3a80e477a60c1ba1cdb2efb9927a161fb78a9bd3
hanumoka <amagramer@gmail.com>
2021-05-06 13:17:21 +0900

    프로필 페이지 만들기 중

---
+++ front/pages/components/FollowList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FollowList = () => {
+    return (<>
+        FollowList
+    </>)
+}
+
+export default  FollowList;
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -2,14 +2,23 @@ import React from "react"
 import AppLayout from "./components/AppLayout";
 import Head from "next/head";
 
+import NicknameEditForm from "./components/NicknameEditForm";
+import FollowList from "./components/FollowList";
+
 const Profile = () => {
+
+    const follwerList = [{nickname: "kyb"}, {nickname: "hanumoka"}, {nickname: "amagramer"}];
+    const follwingList = [{nickname: "kyb"}, {nickname: "hanumoka"}, {nickname: "amagramer"}];
+
     return (
         <>
             <Head>
                 <title>node bird | 프로필</title>
             </Head>
             <AppLayout>
-                <div>내 프로필</div>
+                <NicknameEditForm></NicknameEditForm>
+                <FollowList header="팔로잉 목록" data={follwingList}></FollowList>
+                <FollowList header="팔로워 목록" data={follwerList}></FollowList>
             </AppLayout>
         </>
     )
